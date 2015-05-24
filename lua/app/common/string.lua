29d2c77cbb974eb233b234e99be9f13d1dae6c0a
zhao xiaohua <xiaohua.zhao@alibaba-inc.com>
2015-05-24 14:52:27 +0800

    update

--- lua/app/common/string.lua
+++ lua/app/common/string.lua
@@ -1,11 +1,7 @@
-
-
 local ok, new_tab = pcall(require, "table.new")
 if not ok then
 	new_tab = function (narr, nrec) return {} end
 end
-
-
 local _M = new_tab(0, 16)
 
 --分割字符串
@@ -22,5 +18,54 @@ function _M.split(inputstr, sep)
         return t
 end
 
-
+--HTML字符串过滤
+-- from  kernelsauce/turbo
+_M.escape  = function(s)
+    return (string.gsub(s, "[}{\">/<'&]", {
+        ["&"] = "&amp;",
+        ["<"] = "&lt;",
+        [">"] = "&gt;",
+        ['"'] = "&quot;",
+        ["'"] = "&#039;",
+        ["/"] = "&#047;"
+    }))
+end
+--HTML字符串还原
+-- from  lapis
+local html_unescape_entities = { 
+  		['&amp;'] = '&',
+  		['&lt;'] = '<',
+  		['&gt;'] = '>',
+  		['&quot;'] = '"',
+ 		["&#039;"] = "'",
+  		["&#047;"] = "/"
+	}
+_M.unescape  = function(text)
+  	return (text:gsub("(&[^&]-;)", function(enc)
+    local decoded = html_unescape_entities[enc]
+    if decoded then
+      return decoded
+    else
+      return enc
+    end
+  end))
+end
+-- 去掉空格
+-- from  kernelsauce/turbo
+function _M.trim(s)
+    -- from PiL2 20.4
+    return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+-- 去掉左空格
+-- from  kernelsauce/turbo
+function _M.ltrim(s)
+    return (s:gsub("^%s*", ""))
+end
+-- 去掉右空格
+-- from  kernelsauce/turbo
+function _M.rtrim(s)
+    local n = #s
+    while n > 0 and s:find("^%s", n) do n = n - 1 end
+    return s:sub(1, n)
+end
 return _M
--- lua/app/index.lua
+++ lua/app/index.lua
@@ -1,15 +1,12 @@
 --[[
-	
+	https://github.com/openresty/lua-nginx-module/issues/208
 --]]            
---https://github.com/openresty/lua-nginx-module/issues/208
 require = require"app.common.require".require 
---ngx.say("hello!")
-local ok, dispatcher  = require "app.common.dispatcher"
+--local ok, dispatcher  = require "app.common.dispatcher"
+local s = require "app.common.string"
 
---local ok, dispatcher  = pcall(require, "app.test.nginx")
---ngx.exit(ngx.HTTP_OK)
+ngx.say(s.unescape("1&amp;2"))
 
+ngx.say(s.escape("1'2"))
 
-
---local redis =  require "app.controller.user"
-            
+--ngx.say(s._unescape(s._escape("1&2")))
